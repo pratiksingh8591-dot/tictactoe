e0dc7ada2143c9578350862902ea30a259428e34
Pratik-singh <pratiksingh8591@gmail.com>
2026-02-22 01:46:44 +0530

    created a blueprint

---
+++ app.js
@@ -0,0 +1,46 @@
+let boxes=document.querySelectorAll("#box");
+let resetbtn=document.querySelector("#reset-btn");
+let winningpattern=[[0,1,2] ,[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6] ];
+let winmssg=document.querySelector(".winmssg.hide");
+
+let turn0=true;
+let winner="none";
+
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        console.log("button was clicked")
+        if(turn0){
+            box.innerText="0"
+            turn0=false;
+        }
+        else{
+            box.innerText="X"
+            turn0=true;
+        }
+        box.disabled=true;
+        checkwinner();
+    })
+
+});
+
+
+
+ let checkwinner=() =>{
+    for (let pattern of winningpattern){
+        let pos1val=boxes[pattern[0]].innerText;
+        let pos2val=boxes[pattern[1]].innerText;
+        let pos3val=boxes[pattern[2]].innerText;
+        if(pos1val!="" && pos2val!="" && pos3val!=""){
+        if(pos1val===pos2val && pos2val===pos3val){
+            winner=pos1val;
+            win();
+        }
+    }
+    }
+    
+}
+let win=()=>{
+  winpara.innerText=`the winner is ${winner}`;
+  winmssg.classList.remove("hide");
+  }
