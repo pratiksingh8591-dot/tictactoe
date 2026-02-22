c2e37128e00f2b207320094314941ed16937b02f
Pratik-singh <pratiksingh8591@gmail.com>
2026-02-22 12:49:47 +0530

    tic tac toe

--- app.js
+++ app.js
@@ -1,81 +1,101 @@
-let boxes=document.querySelectorAll("#box");
-let resetbtn=document.querySelector("#reset-btn");
-let winmssg=document.querySelector(".winmssg.hide");
-let winpara=document.querySelector("p");
-let newgame=document.querySelector("#new-game");
+let boxes = document.querySelectorAll(".box");
+let winmssg = document.querySelector(".winmssg");
+let winpara = document.querySelector("p");
+let newgame = document.querySelector("#new-game");
+
+let winningpattern = [
+  [0,1,2],[3,4,5],[6,7,8],
+  [0,3,6],[1,4,7],[2,5,8],
+  [0,4,8],[2,4,6]
+];
+
+let turnO = true;
+let winner = "none";
+let count = 0;
 
 
-let winningpattern=[[0,1,2] ,[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6] ];
-
-let turn0=true;
-let winner="none";
-let count=0;
-
-
+// CLICK EVENT
 boxes.forEach((box)=>{
-    box.addEventListener("click",()=>{
-        console.log("button was clicked")
-        if(turn0){
-            box.innerText="0"
-            turn0=false;
-            count++;
-        }
-        else{
-            box.innerText="X"
-            turn0=true;
-            count++;
-        }
-        box.disabled=true;
-        if(count!=9){ checkwinner();}
-        else{ checkdraw();}
-       
-    })
+  box.addEventListener("click",()=>{
 
+    if(turnO){
+      box.innerText = "O";
+      turnO = false;
+    }else{
+      box.innerText = "X";
+      turnO = true;
+    }
+
+    box.disabled = true;
+    count++;
+
+    checkGame(); // check winner or draw
+  });
 });
 
 
+// CHECK GAME STATUS
+let checkGame = () => {
+  if(checkwinner()) return;
 
- let checkwinner=() =>{
-    for (let pattern of winningpattern){
-        let pos1val=boxes[pattern[0]].innerText;
-        let pos2val=boxes[pattern[1]].innerText;
-        let pos3val=boxes[pattern[2]].innerText;
-        if(pos1val!="" && pos2val!="" && pos3val!=""){
-        if(pos1val===pos2val && pos2val===pos3val){
-            winner=pos1val;
-            win();
-            stop();
-        }
-        }
+  if(count === 9){
+    checkdraw();
+  }
+};
 
+
+// CHECK WINNER
+let checkwinner = () => {
+  for(let pattern of winningpattern){
+
+    let pos1 = boxes[pattern[0]].innerText;
+    let pos2 = boxes[pattern[1]].innerText;
+    let pos3 = boxes[pattern[2]].innerText;
+
+    if(pos1 !== "" && pos1 === pos2 && pos2 === pos3){
+      winner = pos1;
+      showWinner();
+      stopGame();
+      return true; // stop checking further
     }
-    }
- let checkdraw =()=>{
-    draw();
-    stop();
- }
+  }
+  return false;
+};
 
-let win=()=>{
-  winpara.innerText=`the winner is ${winner}`;
+
+// SHOW WINNER
+let showWinner = () => {
+  winpara.innerText = `Congratulations!! Winner is ${winner}`;
   winmssg.classList.remove("hide");
-  }
-  let stop=() =>{
-    for(let box of boxes){
-        box.disabled=true;
-    }
-  }
-  let anewgame=()=>{
-     for(let box of boxes){
-        box.disabled=false;
-        box.innerText="";
-        turn0=true;
-        winmssg.classList.add("hide");
-    }
-  }
-  newgame.addEventListener("click",()=>{
-    anewgame();
-  })
-  let draw=()=>{
-     winpara.innerText=`the Game is draw`;
+};
+
+
+// DRAW
+let checkdraw = () => {
+  winpara.innerText = "It's a DRAW";
   winmssg.classList.remove("hide");
-  }
+  stopGame();
+};
+
+
+// STOP GAME
+let stopGame = () => {
+  boxes.forEach(box => box.disabled = true);
+};
+
+
+// NEW GAME / RESET
+let resetGame = () => {
+  count = 0;
+  winner = "none";
+  turnO = true;
+
+  boxes.forEach(box=>{
+    box.innerText = "";
+    box.disabled = false;
+  });
+
+  winmssg.classList.add("hide");
+};
+
+newgame.addEventListener("click", resetGame);
